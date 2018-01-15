7597826400ad8611e37273b6ea5818ef5374455f
sayantikabanik <sayantikabanik@gmail.com>
2018-01-15 21:25:29 +0530

    updated

--- chat.py
+++ chat.py
@@ -1,7 +1,9 @@
 #!/bin/env python
 from app import create_app, socketio
+import os
+port= int(os.environ.get('PORT',5000))
 
 app = create_app(debug=True)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host='0.0.0.0',port=port)
